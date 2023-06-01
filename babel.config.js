9253ad2696c6c94b65134643cc52990ae0eba5d8
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-06-01 16:16:40 +0200

    Use React's automatic runtime when transpiling

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    require.resolve("@babel/preset-react"),
+    [require.resolve("@babel/preset-react"), { runtime: "automatic" }],
     [
       require.resolve("@babel/preset-typescript"),
       { allExtensions: true, disallowAmbiguousJSXLike: true, isTSX: true },
