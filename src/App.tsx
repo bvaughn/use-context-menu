fd289490712734670f885854460afe4ce19f8e38
Brian Vaughn <brian.david.vaughn@gmail.com>
2025-12-29 13:14:50 -0500

    Hide versions page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import "../styles.css";
 export default function App() {
   return (
     <AppRoot
+      hideVersions
       navLinks={
         <>
           <NavLink path="/">Getting started</NavLink>
